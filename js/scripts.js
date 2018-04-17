3fa4006142b82900d870a7d7123bfc51b47356f5
olaftarlowski <olaf.tarlowski@gmail.com>
2018-04-17 21:02:42 +0200

    initial

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(".scale-img-onclick").each(function(){
+    $(this).click(function(){
+      console.log($(this));  
+        if($(this).hasClass('enlarged')){
+            $(this).removeClass('enlarged');
+            $(this).stop().animate({width: 280, height: 187}, 200);
+        }else{
+            $(this).addClass('enlarged');
+            $(this).stop().animate({width: 800, height: 533}, 200);
+        }
+        
+    })
+});
